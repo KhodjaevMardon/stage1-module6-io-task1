91239d2b8bfc76d76046381f6b754ecbee5efb21
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-17 01:10:00 +0500

    finished the task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+            String content = new String(fileInputStream.readAllBytes());
+            String[] splitByLines = content.split("\n");
+            for (String line : splitByLines) {
+                String[] splitBySemicolon = line.split(":");
+                switch (splitBySemicolon[0]) {
+                    case "Name":
+                        profile.setName(splitBySemicolon[1].trim());
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(splitBySemicolon[1].trim()));
+                        break;
+                    case "Email":
+                        profile.setEmail(splitBySemicolon[1].trim());
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(splitBySemicolon[1].trim()));
+                        break;
+                }
+            }
+            fileInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profile;
     }
 }
